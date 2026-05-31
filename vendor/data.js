727af0b1e42dcc4ca72944e718f964217da7262f
dev-nirob1 <dev.nirob1@gmail.com>
2026-05-31 21:26:26 +0600

    json data added

---
+++ vendor/data.js
@@ -0,0 +1,356 @@
+export const testimonials = [
+    {
+        id: 1,
+        review: "Awesome job every single time I work with him. We have been working together for over a year now. Great experience every single time.",
+        userName: "kenyaparks",
+        country: "United States",
+        image: "https://ui-avatars.com/api/?name=Ken&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 2,
+        review: "Very friendly and professional contact. I am exited how the website will look like. I let you know.",
+        userName: "andreoehsen",
+        country: "Germany",
+        image: "https://ui-avatars.com/api/?name=andre&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 3,
+        review: "The seller communicates and takes direction well. He his good and has good knowledgeable. Good value for money. I will work with him again. Happiness 😀",
+        userName: "omshreembrzee",
+        country: "United Kingdom",
+        image: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/profile/photos/43922613/original/_MG_8549_1.jpg",
+        rating: 5
+    },
+    {
+        id: 15,
+        review: "I am pleased again. This seller keeps solving a lot of riddles within the world of webwork for me. And I am happy to come back again and again, while we have finished many different projects. Thumbs up for him, and: Thaank you :-)",
+        userName: "barbara2",
+        country: "Germany",
+        image: "https://ui-avatars.com/api/?name=barbara2&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 4,
+        review: "we have no idea how to make html page for one industrial product, but seller give us some great ideas and make template for us very fast. thank you very much, will order again!",
+        userName: "avelettta",
+        country: "Estonia",
+        image: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/attachments/profile/photo/394fe8799b64583ca19823baa2812d81-1622209337617/2eb081d6-f082-4ad9-882a-5696a0db387a.jpg",
+        rating: 5
+    },
+    {
+        id: 5,
+        review: "This is is the best. Attention to detail. No complaints, does as instructed and unlimited revisions. Very responsive landing page as well",
+        userName: "franklawson488",
+        country: "Ghana",
+        image: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/attachments/profile/photo/bcf0476bce84db971de13ea639c52bdc-1519481126797/355eff90-e307-4556-aac0-2463996a3883.jpg",
+        rating: 5
+    },
+    {
+        id: 6,
+        review: "Seller exceeded my expectations. He was very cooperative and patient--even when I took a while to reply. Fiverr should have an option to add bonuses when sellers go above and beyond... I will definitely choose this seller for my next project. A+",
+        userName: "user72803480",
+        country: "United States",
+        image: "https://ui-avatars.com/api/?name=user&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 7,
+        review: "Webpoka has real talent !!! He is really good making landing pages i have used his service 4 times so far ! I am happy every time!!!!!! and will continue to work with him. Thank you so much BEST GIG ON FIVVER",
+        userName: "zackjay",
+        country: "Canada",
+        image: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/attachments/profile/photo/bcf0476bce84db971de13ea639c52bdc-1519481126797/355eff90-e307-4556-aac0-2463996a3883.jpg",
+        rating: 5
+    },
+    {
+        id: 8,
+        review: "Fantastic work and very easy to work with. The seller is a great communicator and delivers exactly what you want. Will use again in the future.",
+        userName: "docdigit",
+        country: "United States",
+        image: "https://ui-avatars.com/api/?name=docdigit&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 9,
+        review: "Was a very pleasant experience working with Webpoka. He was always willing to work with me to ensure that I got the result I wanted. He was extremely cooperative, which I truly appreciated.",
+        userName: "thejourneyer",
+        country: "Barbados",
+        image: "https://ui-avatars.com/api/?name=thejourneyer&background=0D8ABC&color=fff",
+        rating: 4
+    },
+    {
+        id: 10,
+        review: "I received a creative landing page just as requested and this seller was very easy to talk to. I would recommend him to anyone.",
+        userName: "jessicarushing",
+        country: "United States",
+        image: "https://ui-avatars.com/api/?name=jessicarushing&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 11,
+        review: "Very Satisfied with the seller, extremely patient and a breeze to work with. Will certainly avail Afroj's services again. Keep up the good work",
+        userName: "clearmind28",
+        country: "India",
+        image: "https://ui-avatars.com/api/?name=clearmind28&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 12,
+        review: "Absolutely brilliant job and exactly what I expected. Seller went above and beyond to deal with my very specific revisions. Will definitely use this service again.",
+        userName: "tchapwise",
+        country: "United Kingdom",
+        image: "https://ui-avatars.com/api/?name=tchapwise&background=0D8ABC&color=fff",
+        rating: 5
+    },
+    {
+        id: 13,
+        review: "Hes Great, Experienced, and highly qualified person who knows how to solve the problems.. Great Work",
+        userName: "deepakkew",
+        country: "United Arab Emirates",
+        image: "https://fiverr-res.cloudinary.com/image/upload/f_auto,q_auto,t_profile_small/v1/profile/photos/47827403/original/home-style-logo.jpg",
+        rating: 5
+    },
+    {
+        id: 14,
+        review: "Due to not having wordpress not sure was worth doing however help to set up existing landing page. Seller was good communicator and patient with my many questions",
+        userName: "skurrie",
+        country: "Australia",
+        image: "https://ui-avatars.com/api/?name=skurrie&background=0D8ABC&color=fff",
+        rating: 4
+    }
+
+]
+export const projects = [
+    // LANDING PAGES
+    {
+        id: 1,
+        title: "Real Estate Landing Page",
+        category: "landing-page",
+        liveLink: "https://cityview-landing-page.netlify.app/",
+        image: "/projectImage/cityview.png"
+    },
+    {
+        id: 2,
+        title: "Bag Selling Landing Page",
+        category: "landing-page",
+        liveLink: "https://bagpoint.netlify.app/",
+        image: "/projectImage/bagpoint.png"
+    },
+    {
+        id: 3,
+        title: "Pizza Slice Landing Page",
+        category: "landing-page",
+        liveLink: "https://slicepoint.netlify.app/",
+        image: "/projectImage/slicepoint.png"
+    },
+    {
+        id: 4,
+        title: "Crypto Coin",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/cryptocoin/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/cryptocoin/cryptocoin.avif"
+    },
+    {
+        id: 5,
+        title: "Events",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/events/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/events/events.avif"
+    },
+    {
+        id: 6,
+        title: "Renovation",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/renovation/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/renovation/renovation.avif"
+    },
+    {
+        id: 7,
+        title: "Vesco",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/vesco/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/vesco/vesco.avif"
+    },
+    {
+        id: 8,
+        title: "Ask Me",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/chainese/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/chainese/chainese.avif"
+    },
+    {
+        id: 9,
+        title: "Xocondos",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/xocondos/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/xocondos/xocondos.avif"
+    },
+
+    // FULL STACK
+    {
+        id: 10,
+        title: "Real Estate Website",
+        category: "full-stack",
+        liveLink: "https://scionassetbd.com/",
+        image: "/projectImage/scion-asset.png"
+    },
+
+    // FRONT-END PROJECTS (ORDERED)
+    {
+        id: 11,
+        title: "Event Management Website",
+        category: "front-end",
+        liveLink: "https://the-venue-event-management.netlify.app/",
+        image: "/projectImage/event-management.webp"
+    },
+    {
+        id: 12,
+        title: "Healthcare Website",
+        category: "front-end",
+        liveLink: "https://luxemed-healthcare.netlify.app/",
+        image: "/projectImage/luxemed-healthcare.webp"
+    },
+    {
+        id: 13,
+        title: "E-commerce Website",
+        category: "front-end",
+        liveLink: "https://nebula-store-e-commerce.netlify.app/",
+        image: "/projectImage/nebul-ecommerce.webp"
+    },
+    {
+        id: 14,
+        title: "Gym Website",
+        category: "front-end",
+        liveLink: "https://fitlab-fitness-website.netlify.app/",
+        image: "/projectImage/gym-website.webp"
+    },
+    {
+        id: 15,
+        title: "School Website",
+        category: "front-end",
+        liveLink: "https://kids-green-school.netlify.app/",
+        image: "/projectImage/kids-green.webp"
+    },
+    {
+        id: 16,
+        title: "Real Estate Website",
+        category: "front-end",
+        liveLink: "https://velvet-gold-real-estage.netlify.app/",
+        image: "/projectImage/velvet-gold.webp"
+    },
+    {
+        id: 17,
+        title: "Real Estate Front-end",
+        category: "front-end",
+        liveLink: "https://real-estate-dream-home.netlify.app/",
+        image: "/projectImage/dream-homes.webp"
+    },
+    {
+        id: 18,
+        title: "Mirchi Restaurant Website",
+        category: "front-end",
+        liveLink: "https://mirchi-restaurant.netlify.app/",
+        image: "/projectImage/mirchi-restaurant.webp"
+    },
+    {
+        id: 19,
+        title: "Bakery Website",
+        category: "front-end",
+        liveLink: "https://lumina-bakery-website.netlify.app/",
+        image: "/projectImage/lumina-bakery.webp"
+    },
+
+    // OTHER LANDING PAGES
+    {
+        id: 20,
+        title: "Accounting",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/accounting/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/accounting/accounting.avif"
+    },
+    {
+        id: 21,
+        title: "Clothing",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/clothing/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/clothing/clothing.avif"
+    },
+    {
+        id: 22,
+        title: "CPA",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/cpa/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/cpa/cpa.avif"
+    },
+    {
+        id: 23,
+        title: "E-Book",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/ebook/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/ebook/ebook.avif"
+    },
+    {
+        id: 24,
+        title: "Restaurant",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/restaurantf/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/restaurantf/restaurantf.avif"
+    },
+    {
+        id: 25,
+        title: "E-Book 4",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/ebook4/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/ebook4/ebook4.avif"
+    },
+    {
+        id: 26,
+        title: "Real Estate Zack",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/realestate_zack/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/realestate_zack/realestate_zack.avif"
+    },
+    {
+        id: 27,
+        title: "Real Estate CMS",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/realestate_cms/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/realestate_cms/realestate_cms.avif"
+    },
+    {
+        id: 28,
+        title: "Real Home",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/real_home/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/real_home/real_home.avif"
+    },
+    {
+        id: 29,
+        title: "Problems.Fyi",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/problems.fyi/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/problems.fyi/problems.fyi.avif"
+    },
+    {
+        id: 30,
+        title: "Limo Service",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/limo_service/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/limo_service/limo_service.avif"
+    },
+    {
+        id: 31,
+        title: "Forex Bonus",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/forex_bonus/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/forex_bonus/forex_bonus.avif"
+    },
+    {
+        id: 32,
+        title: "E-Book 2",
+        category: "landing-page",
+        liveLink: "https://portfolio.webpoka.com/projects/landing_page/ebook2/",
+        image: "https://portfolio.webpoka.com/projects/landing_page/ebook2/ebook2.avif"
+    }
+]
