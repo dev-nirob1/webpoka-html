864634f36c236488a3c098605b2494c1055cdacf
dev-nirob1 <dev.nirob1@gmail.com>
2026-06-01 22:19:53 +0600

    image viewer modal

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const openPopupBtns = document.querySelectorAll('.popup-btn');
     const popupCloseBtn = document.getElementById('popup-close');
 
+    const imageBtn = document.querySelectorAll('.image-popup');
+    const imageCloseBtn = document.querySelector('.close-btn');
+    const modal = document.querySelector('.modal');
+
+    modal.style.display = 'none'; // hide image popup on page load
+    const images = document.querySelectorAll('.image-popup');
+    const modalImage = document.querySelector('.modal-details .image img');
+    let currentImageIndex = 0;
+    images.forEach((img, index) => {
+        img.addEventListener('click', () => {
+            modal.style.display = 'flex';
+            currentImageIndex = index;
+            updateModalImage();
+        });
+    });
+    const updateModalImage = () => {
+        modalImage.src = images[currentImageIndex].src;
+    };
+    document.querySelector('.btn-next').addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        updateModalImage();
+    });
+    document.querySelector('.btn-prev').addEventListener('click', () => {
+        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+        updateModalImage();
+    });
+    imageCloseBtn.addEventListener('click', () => {
+        modal.style.display = 'none';
+    });
+
     // popup toggle
     popup.style.display = 'none'; // hide popup on page load
     openPopupBtns.forEach(btn => {
